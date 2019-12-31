a3d1b1ece0b6897499408fdb72b79682878bd98c
huweidong <466463054@qq.com>
2019-12-31 12:12:19 +0800

    456

--- app/src/main/java/com/guoyu/fusemanagerapp/net/NetUrl.java
+++ app/src/main/java/com/guoyu/fusemanagerapp/net/NetUrl.java
@@ -5,9 +5,9 @@ package com.guoyu.fusemanagerapp.net;
  */
 
 public class NetUrl {
-    public static final String BASE_URL = "http://192.168.1.102:8080/";
+//    public static final String BASE_URL = "http://192.168.1.102:8080/";
 //    public static final String BASE_URL = "http://122.112.187.234:8888/";
-//    public static final String BASE_URL = "http://39.98.188.171:8088/";
+    public static final String BASE_URL = "http://39.98.188.171:8088/";
     public static final String H5BASE_URL = "http://xfsysh5.5ijiaoyu.cn/";
 //    public static final String BASE_URL = "http://112.101.203.127:9999/";
     //app商户登录/管理员登录
